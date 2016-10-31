4a87cfb4cafebd839f573699e9bb8fe2754d87e3
LCattle <linguohui@yeah.net>
2016-10-31 09:43:27 +0800

    modify

--- js/cattle.red.pack2.js
+++ js/cattle.red.pack2.js
@@ -1,110 +1,104 @@
 /**
  * Created by cattle on 2016/10/18 0018.
  */
-(function(window, document, $){
+(function (window, document, $) {
     $.fn._RP = {
-        init : function(options){
+        init: function (options) {
             this.defaults = {
-                ele : '.red-pack-box',
-                domSwitch: true,
-                winW : $(window).width(),
-                winH : $(window).height(),
-                downPositions :['item-left', 'item-right', 'item-center'],
-                red_pack_W: 0,
-                red_pack_H: 0
+                ele: '.red-pack-box', //红包的box
+                domSwitch: true,    //是否开始加载红包
+                winW: $(window).width(),   //浏览器宽度
+                winH: $(window).height(), //浏览器高度
+                red_pack_types: ['box-item-big', 'box-item-small'], //红包大小
+                red_pack_W: 0, //红包的宽度
+                red_pack_H: 0,  //红包的高度
+                load_red_pack_time: 200, //每个红包的间隔时间
+                animation_time: 2000    //红包执行落下完成的时间
             }
-
             this.options = $.extend({}, this.defaults, options);
             $(this.options.ele).css({
-                width : this.options.winW,
-                height : this.options.winH
+                width: this.options.winW,
+                height: this.options.winH
             });
             $('.red-pack-pup').css({
-                width : this.options.winW,
-                height : this.options.winH
+                width: this.options.winW,
+                height: this.options.winH
             });
             this.loadRedPack(this.options.ele);
         },
-
         /**
-         * 加载HTML
-         * @param ele
+         * 加载红包HTML
+         * @param ele 调用的元素
          */
-        loadRedPack : function(ele){
+        loadRedPack: function (ele) {
             var self = this;
-            var redPackHtml = '';
-            var ran = '';
-            var redPackDown = '';
-            var i = 0;
-            var loadRedPackTime = window.setInterval(function(){
-                i++;
-                ran = self.countRandom(3)
-                redPackHtml = '<div class="box-item '+ self.options.downPositions[ran] +'" data-rp-id = "'+ i +'"></div>';
-                $(ele).append(redPackHtml);
-                redPackDown = $('div[data-rp-id="'+ i +'"]');
-                //为每个红包设定大小
-                self.setPackSize(redPackDown);
-                //为每个红包设定top值
-                self.packMarginTop(redPackDown);
-                self.start(redPackDown);
-            }, 200);
-
-
-
-
-        },
-        /**
-         * 为每个红包设定大小
-         * @param ele
-         */
-        setPackSize: function(ele){
-            if($(ele).hasClass('item-left') || $(ele).hasClass('item-right')){
-                $(ele).addClass('box-item-small');
+            if (self.options.domSwitch) {
+                var redPackHtml = '';
+                var ran = '';
+                var redPackDown = '';
+                var i = 0;
+                var loadRedPackTime = window.setInterval(function () {
+                    i++;
+                    ran = self.countRandom(2)
+                    redPackHtml = '<div class="box-item ' + self.options.red_pack_types[ran] + '" data-rp-id = "' + i + '"></div>';
+                    $(ele).append(redPackHtml);
+                    redPackDown = $('div[data-rp-id="' + i + '"]');
+                    self.lotteryDraw(redPackDown);
+                    //为每个红包设定top值
+                    self.packMarginTop(redPackDown);
+                    self.start(redPackDown);
+                }, self.options.load_red_pack_time);
             }
         },
         /**
          * 为每个红包设定偏移值
          * @param ele
          */
-        packMarginTop: function(ele){
+        packMarginTop: function (ele) {
             var self = this;
             var _t = $(ele);
-            self.options.red_pack_H =  _t.outerHeight(true);
-            self.options.red_pack_W = _t.outerHeight(true)
+            self.options.red_pack_H = _t.outerHeight(true);
+            self.options.red_pack_W = _t.outerHeight(true);
             //计算出红包dom占用了浏览器的百分几
-            var occupyPost = (self.countRandom(Math.floor(self.options.winW /  self.options.red_pack_W)));
-            //console.log('occupyPost:' + occupyPost);
+            var occupyPost = (self.countRandom(Math.floor(self.options.winW / self.options.red_pack_W)));
             _t.css({
-                'top': '-' +  self.options.red_pack_H + 'px',
-                'left': (occupyPost *  self.options.red_pack_W) + 'px'
+                'top': '-' + self.options.red_pack_H + 'px',
+                'left': (occupyPost * self.options.red_pack_W) + 'px'
             })
         },
         /**
          * 执行落下动作
          * @param ele
          */
-        start: function(ele){
+        start: function (ele) {
             var self = this;
-            if(self.options.domSwitch){
-                var _t = $(ele);
-                _t.stop(true, true).animate({
-                    'top':(self.options.winH + _t.height())
-                }, 2000, function(){
-                    _t.remove();
-                });
-
-            }
+            var _t = $(ele);
+            _t.stop(true, true).animate({
+                'top': (self.options.winH + self.options.red_pack_H)
+            }, self.options.animation_time, function () {
+                _t.remove();
+            });
         },
+        /**
+         * 点击红包
+         * @param ele
+         */
+        lotteryDraw: function (ele) {
+            $(ele).unbind('click').on('click', function () {
+                var _t = $(ele);
+               
 
+            });
+        },
         /**
          * 随机数
-         * @param b 结束的数
-         * @param e 开始的数
+         * @param n 返回的最高数
          * @returns {number}
          */
-        countRandom: function(n){
+        countRandom: function (n) {
             return parseInt(Math.floor(n * Math.random()));
+        },
+        initPupHtml: function () {
         }
     }
-
-})(window, document, jQuery)
+})(window, document, jQuery);
